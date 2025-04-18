e8c2ec24c13540a1a62b45fe63d16e237bd40e55
sagarbgohil <gohilbsagar@gmail.com>
2025-04-18 17:29:20 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Updated React and React DOM to version 19.1.0
    - Added several Radix UI components and utility libraries
    - Introduced Tailwind CSS configuration file with custom theme settings
    - Updated ESLint and TypeScript definitions for better development experience

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetBrainsMono)",
+    },
+    extend: {
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+      },
+
+      fontFamily: {
+        primary: "var(--font-jetBrainsMono)",
+      },
+    },
+  },
+};
