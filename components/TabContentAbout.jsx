72fe4c867e04e5243f0780634797fd79ac7a2fdd
sagarbgohil <gohilbsagar@gmail.com>
2025-04-20 14:49:35 +0530

    fix: update TabContentAbout component to include text alignment styles

--- components/TabContentAbout.jsx
+++ components/TabContentAbout.jsx
@@ -3,7 +3,7 @@ import { ScrollArea } from "./ui/scroll-area";
 
 const TabContentAbout = ({ data }) => {
   return (
-    <div className="flex flex-col gap-[30px]">
+    <div className="flex flex-col gap-[30px] text-center xl:text-left">
       <h2 className="text-4xl font-bold">{data.title}</h2>
       <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
         {data.description}
