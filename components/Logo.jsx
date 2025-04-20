3aba053094df97f9ca02118eca6ed8d6cad16c0c
sagarbgohil <gohilbsagar@gmail.com>
2025-04-20 14:56:22 +0530

    fix: update title and description in metadata and Logo component to reflect correct role as Software Engineer

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
     <div>
       <Link href="/">
         <h1 className="hidden">
-          Sagar Gohil | Backend Engineer and Tech Enthusiast
+          Sagar Gohil | Software Engineer and Tech Enthusiast
         </h1>
         <h2 className="h1 text-4xl font-semibold">
           SG<span className="text-accent">.</span>
