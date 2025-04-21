f39c30e4aecdc8b59b9c9c2829723e0dd67d6f48
sagarbgohil <gohilbsagar@gmail.com>
2025-04-21 22:24:45 +0530

    feat: update StickyWidget image source and adjust width; add media2.giphy.com to image domains

--- components/StickWidget.jsx
+++ components/StickWidget.jsx
@@ -13,9 +13,10 @@ const StickyWidget = () => {
         className="block"
       >
         <Image
-          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+          // src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+          src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmo4dnY2b3V5ZWZpOHRmcWNiZHVyNXJpbGNocXl6Z2k0YmhqcTZveSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/TDQOtnWgsBx99cNoyH/giphy.gif"
           alt="Buy Me A Coffee"
-          width={160}
+          width={50}
           height={50}
           className="rounded-md shadow-lg hover:scale-105 transition-transform duration-300"
         />
